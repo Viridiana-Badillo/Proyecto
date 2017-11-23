4b5d82eb8a96512d8f41e36efcfc0d05bc8e21a3
David Garfias Aznar <jdgarfiasaznar@gmail.com>
2017-11-22 21:53:44 -0600

    Update graficar.py
    Cambio en el formato de la gráfica.

--- graficar.py
+++ graficar.py
@@ -2,8 +2,7 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 import matplotlib.pyplot as plt
-from decimal import * 
-import datetime
+import time
 
 # Abrimos el archivo de la comunicacón
 archivo = open("comunicacion.txt", "r")
@@ -26,31 +25,47 @@ tipo = len(terminos) - 1
 
 # Leemos el rango para graficar
 print("Dame el rango para graficar [x1, x2]")
-x1 = float(raw_input(" x1: "))
-x2 = float(raw_input(" x2: "))
+x1 = float(input(" x1: "))
+x2 = float(input(" x2: "))
 
 # Rango de la función
 x = np.arange(x1, x2)
+
+# Damos el formato para poder graficar 
+fig = plt.figure()
 
 # Dependiendo del tipo de ecuación es la función
 if tipo == 1:
 	y = (x * terminos[0]) +  terminos[1]
 	# u es unicode
-	plt.title(u"{0}x + {1} = 0".format(terminos[0], terminos[1]))
+	# {:g} trunca los ceros sobrantes y los puntos inecesarios en los números
+	# fontsize y fontweight dan formato al título
+	fig.suptitle(u"{:g}x + {:g} = 0".format(terminos[0], terminos[1]),
+		fontsize=14, fontweight='bold')
 elif tipo == 2:
-	y = ((x**2) * terminos[0]) + (x * terminos[1])
-	y += Decimal(terminos[2])
-
-	plt.title(u"{0}x² + {1}x + {2} = 0".format(
-		terminos[0], terminos[1], terminos[2]))
+	y = ((x**2) * terminos[0]) + (x * terminos[1]) + terminos[2]
+	fig.suptitle(u"{:g}x² + {:g}x + {:g} = 0".format(
+		terminos[0], terminos[1], terminos[2]), fontsize=14, fontweight='bold')
 elif tipo == 3:
 	y = (x**3 * terminos[0]) + ((x**2) * terminos[1])
 	y += (x * terminos[2]) + terminos[3]
-	plt.title(u"{0}x³ + {1}x² + {2}x + {3} = 0".format(
-		terminos[0], terminos[1], terminos[2], terminos[3]))
+	fig.suptitle(u"{:g}x³ + {:g}x² + {:g}x + {:g} = 0".format(
+		terminos[0], terminos[1], terminos[2], terminos[3]),
+		fontsize=14, fontweight='bold')
 
 # Graficamos, formateamos y guardamos con nombre de la fecha y hora
 plt.plot(x, y)
 plt.grid()
-time = datetime.datetime.now().strftime("%Y-%m-%d %H·%M")
-plt.savefig("graficas/{0}.png".format(time))
+
+# Ruta donde se va a guardar el archivo
+nombre = "graficas/{0}-{1}-{2} {3}:{4}:{5}.png".format(
+	time.strftime('%Y'),
+	time.strftime('%m'),
+	time.strftime('%d'),
+	time.strftime('%H'),
+	time.strftime('%M'),
+	time.strftime('%S'))
+
+# Guardamos y avisamos de ello
+plt.savefig(nombre)
+print("Tú gráfica se ha guardado en \"{0}\"".format(nombre))
